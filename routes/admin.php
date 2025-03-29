40fd078a9a370f8d0afb4e94f3e74fafae146cc5
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-03-29 07:20:52 +0200

    Admin Dashboard Basic views and Controllers

--- routes/admin.php
+++ routes/admin.php
@@ -2,19 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
 
-require __DIR__.'/auth.php';
+
