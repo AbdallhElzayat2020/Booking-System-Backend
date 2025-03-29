b2408613af1051f53cd36c8d42c1e88268f188a5
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-03-29 17:16:17 +0200

    Login page with custome view and make it dynamic

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,59 +1,9 @@
-{{--<x-guest-layout>--}}
-{{--    <!-- Session Status -->--}}
-{{--    <x-auth-session-status class="mb-4" :status="session('status')" />--}}
-
-{{--    <h3>Admin Login</h3>--}}
-{{--    <form method="POST" action="{{ route('login') }}">--}}
-{{--        @csrf--}}
-
-{{--        <!-- Email Address -->--}}
-{{--        <div>--}}
-{{--            <x-input-label for="email" :value="__('Email')" />--}}
-{{--            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />--}}
-{{--            <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <!-- Password -->--}}
-{{--        <div class="mt-4">--}}
-{{--            <x-input-label for="password" :value="__('Password')" />--}}
-
-{{--            <x-text-input id="password" class="block mt-1 w-full"--}}
-{{--                          type="password"--}}
-{{--                          name="password"--}}
-{{--                          required autocomplete="current-password" />--}}
-
-{{--            <x-input-error :messages="$errors->get('password')" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <!-- Remember Me -->--}}
-{{--        <div class="block mt-4">--}}
-{{--            <label for="remember_me" class="inline-flex items-center">--}}
-{{--                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">--}}
-{{--                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>--}}
-{{--            </label>--}}
-{{--        </div>--}}
-
-{{--        <div class="flex items-center justify-end mt-4">--}}
-{{--            @if (Route::has('admin.password.request'))--}}
-{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('admin.password.request') }}">--}}
-{{--                    {{ __('Forgot your password?') }}--}}
-{{--                </a>--}}
-{{--            @endif--}}
-
-{{--            <x-primary-button class="ms-3">--}}
-{{--                {{ __('Log in') }}--}}
-{{--            </x-primary-button>--}}
-{{--        </div>--}}
-{{--    </form>--}}
-{{--</x-guest-layout>--}}
-
-
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login &mdash; Stisla</title>
+    <title>Login</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{asset('assets/modules/bootstrap/css/bootstrap.min.css')}}">
@@ -78,8 +28,7 @@
 
         gtag('config', 'UA-94034622-3');
     </script>
-    <!-- /END GA -->
-</head>
+    <!-- /END GA --></head>
 
 <body>
 <div id="app">
@@ -95,21 +44,26 @@
                         <div class="card-header"><h4>Login</h4></div>
 
                         <div class="card-body">
-                            <form method="post" action="#" class="needs-validation" novalidate="">
+                            <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                                 @csrf
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                                    <input id="email" type="email" class="form-control" value="{{old('email')}}" name="email" tabindex="1" required autofocus>
                                     <div class="invalid-feedback">
                                         Please fill in your email
                                     </div>
+                                    @if($errors->get('email'))
+                                        <code>
+                                            {{$errors->first('email')}}
+                                        </code>
+                                    @endif
                                 </div>
 
                                 <div class="form-group">
                                     <div class="d-block">
                                         <label for="password" class="control-label">Password</label>
                                         <div class="float-right">
-                                            <a href="#" class="text-small">
+                                            <a href="{{ route('admin.password.request') }}" class="text-small">
                                                 Forgot Password?
                                             </a>
                                         </div>
@@ -118,6 +72,11 @@
                                     <div class="invalid-feedback">
                                         please fill in your password
                                     </div>
+                                    @if($errors->get('password'))
+                                        <code>
+                                            {{$errors->first('password')}}
+                                        </code>
+                                    @endif
                                 </div>
 
                                 <div class="form-group">
@@ -133,13 +92,11 @@
                                     </button>
                                 </div>
                             </form>
+
                         </div>
                     </div>
-                    <div class="mt-5 text-muted text-center">
-                        Don't have an account? <a href="{{ route('register') }}">Create One</a>
-                    </div>
                     <div class="simple-footer">
-                        Copyright &copy; Stisla {{date('Y')}}
+                        Copyright &copy; <a class="text-primary" href="">Abdallh Elzayat</a> {{date('Y')}}
                     </div>
                 </div>
             </div>
@@ -149,12 +106,19 @@
 
 <!-- General JS Scripts -->
 <script src="{{asset('assets/modules/jquery.min.js')}}"></script>
-<script src="{{asset('assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
 <script src="{{asset('assets/modules/popper.js')}}"></script>
+<script src="{{asset('assets/modules/tooltip.js')}}"></script>
+<script src="{{asset('assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
 <script src="{{asset('assets/modules/moment.min.js')}}"></script>
 <script src="{{asset('assets/js/stisla.js')}}"></script>
 
+<!-- JS Libraies -->
+
+<!-- Page Specific JS File -->
+
 <!-- Template JS File -->
-<script src="assets/js/scripts.js"></script>
+<script src="{{asset('assets/js/scripts.js')}}"></script>
+<script src="{{asset('assets/js/custom.js')}}"></script>
 </body>
 </html>
