a9f529abb43e02ffa1e851a7d8261ea919c7294b
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-03-29 10:19:49 +0200

    Necessary files for dashboard admin & remove unuseful files ( js , css )

---
+++ resources/views/admin/layouts/scripts.blade.php
@@ -0,0 +1,15 @@
+<script src="{{asset('assets/modules/jquery.min.js')}}"></script>
+<script src="{{asset('assets/modules/popper.js')}}"></script>
+<script src="{{asset('assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+<script src="{{asset('assets/js/stisla.js')}}"></script>
+
+<!-- JS Libraies -->
+<script src="{{asset('assets/modules/summernote/summernote-bs4.js')}}"></script>
+
+<!-- Page Specific JS File -->
+<script src="{{asset('assets/js/page/index-0.js')}}"></script>
+
+<!-- Template JS File -->
+<script src="{{asset('assets/js/scripts.js')}}"></script>
+@stack('js')
