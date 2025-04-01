9b7f273ee70ffb664d8a17ae9d1d93aff5f26c01
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-04-01 12:44:43 +0200

    Dashboard for User Design

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended(route('admin.dashboard.index', absolute: false));
         }
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('user.dashboard', absolute: false));
     }
 
     /**
