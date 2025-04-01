7819dccbde07363e707f0fb0296404b793082c78
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-04-01 13:00:18 +0200

    Design complete for Normal User

--- resources/views/frontend/dashboard/sidebar.blade.php
+++ resources/views/frontend/dashboard/sidebar.blade.php
@@ -4,15 +4,23 @@
         <img src="images/user_large_img.jpg" alt="logo" class="img-fluid">
     </a>
     <ul class="dashboard_link">
-        <li><a class="active" href="dsahboard.html"><i class="fas fa-tachometer"></i>Dashboard</a></li>
+        <li><a class="active" href="{{ route('user.dashboard') }}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
         <li><a href="dsahboard_listing.html"><i class="fas fa-list-ul"></i> My Listing</a></li>
         <li><a href="dsahboard_create_listing.html"><i class="fal fa-plus-circle"></i> Create Listing</a></li>
         <li><a href="dsahboard_review.html"><i class="far fa-star"></i> Reviews</a></li>
         <li><a href="dsahboard_wishlist.html"><i class="far fa-heart"></i> Wishlist</a></li>
-        <li><a href="dsahboard_profile.html"><i class="far fa-user"></i> My Profile</a></li>
         <li><a href="dsahboard_order.html"><i class="fal fa-notes-medical"></i> Orders</a></li>
         <li><a href="dsahboard_package.html"><i class="fal fa-gift-card"></i> Package</a></li>
         <li><a href="dsahboard_message.html"><i class="far fa-comments-alt"></i> Message</a></li>
-        <li><a href="#"><i class="far fa-sign-out-alt"></i> Logo</a></li>
+        <li><a href="{{ route('user.profile.index') }}"><i class="far fa-user"></i> My Profile</a></li>
+        <li>
+            <form id="logout" action="{{ route('logout') }}" method="post">
+                @csrf
+                <a onclick="event.preventDefault();document.getElementById('logout').submit();" href="{{ route('logout') }}">
+                    <i class="far fa-sign-out-alt"></i>
+                    Logout
+                </a>
+            </form>
+        </li>
     </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,26 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Frontend\DashboardController;
 
+
+
 Route::get('/', [FrontendController::class, 'index'])->name('frontend.index');
 
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
 
 Route::group(['middleware' => 'auth', 'prefix' => 'user', 'as' => 'user.'], function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard'); // user.dashboard
+
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index'); // user.dashboard
 
 });
 
