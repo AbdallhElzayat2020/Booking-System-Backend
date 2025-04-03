cc9b75076f3f0426b1175e27486969a91c9596fd
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2025-04-03 02:06:37 +0200

    Make sidebar Active in sidebar W

---
+++ app/Helpers/GlobalHelper.php
@@ -0,0 +1,10 @@
+<?php
+function setSidebarActive(array $routes): ?string
+{
+    foreach ($routes as $route) {
+        if (request()->routeIs($route)) {
+            return 'active';
+        }
+    }
+    return '';
+}
--- resources/views/frontend/home/sections/banner-section.blade.php
+++ resources/views/frontend/home/sections/banner-section.blade.php
@@ -2,6 +2,7 @@
     <div class="wsus__banner_overlay">
         <div class="container">
             <div class="row justify-content-between">
+
                 <div class="col-xl-6 col-lg-7">
                     <div class="wsus__banner_text">
                         <h1>{!! @$hero->title !!}</h1>
@@ -12,11 +13,14 @@
                 </div>
 
                 <div class="col-xl-5 col-lg-5">
+
                     <form>
                         <h3>Find the Best Place Title</h3>
+
                         <div class="wsus__search_area">
                             <input type="text" placeholder="What we are looking for?">
                         </div>
+
                         <div class="wsus__search_area">
                             <select class="select_2" name="state">
                                 <option value="">categories</option>
@@ -27,6 +31,7 @@
                                 <option value="">categories 5</option>
                             </select>
                         </div>
+
                         <div class="wsus__search_area">
                             <select class="select_2" name="state">
                                 <option value="">location</option>
@@ -37,9 +42,11 @@
                                 <option value="">location 5</option>
                             </select>
                         </div>
+
                         <div class="wsus__search_area m-0">
                             <a href="#" class="read_btn">search</a>
                         </div>
+
                     </form>
                 </div>
             </div>
